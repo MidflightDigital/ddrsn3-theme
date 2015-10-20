f4abf2fd422765ccfb1d96196b64361a6afa4cb9
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2015-10-19 19:43:10 -0500

    Some metrics tweaks

--- DDR SN3/BGAnimations/ScreenSelectMusic overlay/default.lua
+++ DDR SN3/BGAnimations/ScreenSelectMusic overlay/default.lua
@@ -38,7 +38,7 @@ local t = Def.ActorFrame{
 	Texture="bpm gauge",
 		InitCommand=function(self)
 			self:draworder(100):x(SCREEN_CENTER_X-244):y(SCREEN_CENTER_Y-5)
-			self:diffuse(0.5,0.5,0.5)
+			self:diffuse(0.5,0.5,0.5,1)
 		end,
 		OnCommand=function(self)
 			self:diffusealpha(0):sleep(0.6):draworder(50):diffusealpha(1)
