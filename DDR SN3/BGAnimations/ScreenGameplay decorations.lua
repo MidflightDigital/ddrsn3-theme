7a6b9f9206376f176519daa8f82f550a85c3b739
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2015-10-20 10:19:31 -0500

    Fix a problem that would have happened in course mode Even though course mode can't be selected yet.

--- DDR SN3/BGAnimations/ScreenGameplay decorations.lua
+++ DDR SN3/BGAnimations/ScreenGameplay decorations.lua
@@ -17,11 +17,12 @@ end
 
 t[#t+1] = Def.Actor{
     JudgmentMessageCommand = function(_,params)
+        local stage = GAMESTATE:IsCourseMode() and GAMESTATE:GetCourseSongIndex() + 1 or nil
         local info = ScoringInfo[params.Player]
         if params.HoldNoteScore then
-            info.AddHoldScore(params.HoldNoteScore)
+            info.AddHoldScore(params.HoldNoteScore, stage)
         else
-            info.AddTapScore(params.TapNoteScore)
+            info.AddTapScore(params.TapNoteScore, stage)
         end
         local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(params.Player)
         pss:SetScore(info.GetCurrentScore())
