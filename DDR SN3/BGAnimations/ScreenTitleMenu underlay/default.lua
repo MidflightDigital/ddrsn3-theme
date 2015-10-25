0d521773c9a4698de195caa6412a13c2ab5e0c10
Inorizushi <inorizushi@gmail.com>
2015-10-24 20:26:34 -0700

    make the title menu fancy. sort of.

--- DDR SN3/BGAnimations/ScreenTitleMenu underlay/default.lua
+++ DDR SN3/BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -42,10 +42,6 @@ t[#t+1] = Def.ActorFrame {
 		Def.Quad{
 			InitCommand=cmd(FullScreen;diffuse,color("0,0,0,0.6"));
 		};
-		LoadActor("left_panel")..{
-			InitCommand=cmd(x,SCREEN_LEFT+47;y,SCREEN_CENTER_Y);
-			OnCommand=cmd(addx,-94;decelerate,0.2;addx,94);
-		};
 		LoadActor("image")..{
 			InitCommand=cmd(x,SCREEN_LEFT+182;y,SCREEN_CENTER_Y-40);
 			OnCommand=cmd(zoom,0.2;linear,0.4;zoom,1.3;linear,0.2;zoom,1);
@@ -55,6 +51,11 @@ t[#t+1] = Def.ActorFrame {
 					self:Load(path)
 				end
 			end;
+			OffCommand=cmd(accelerate,1;addx,(SCREEN_WIDTH)-182);
+		};
+		LoadActor("left_panel")..{
+			InitCommand=cmd(x,SCREEN_LEFT+47;y,SCREEN_CENTER_Y);
+			OnCommand=cmd(addx,-94;decelerate,0.2;addx,94);
 		};
 		Def.Quad{
 			InitCommand=cmd(blend,Blend.Add;;x,SCREEN_LEFT+182;y,SCREEN_CENTER_Y-40;scaletoclipped,296,216);
@@ -63,12 +64,15 @@ t[#t+1] = Def.ActorFrame {
 		LoadActor("home_dialog")..{
 			InitCommand=cmd(CenterX;y,SCREEN_BOTTOM-80);
 			OnCommand=cmd(zoomy,0;sleep,0.1;accelerate,0.3;zoomy,1);
+			OffCommand=cmd(zoomy,1;sleep,0.1;accelerate,0.3;zoomy,0);
 		};
 		Def.BitmapText{
 			Font="_russell_square";
 			Text="";
 			InitCommand=function(self) self:hibernate(0.4):Center(X):y(SCREEN_BOTTOM-100):zoom(0.9):maxwidth(513):wrapwidthpixels(513):valign(0):vertspacing(8) end;
-			TitleSelectionMessageCommand=function(self, params) self:settext(THEME:GetString("ScreenTitleMenu","Description"..params.Choice)) end
+			TitleSelectionMessageCommand=function(self, params) self:settext(THEME:GetString("ScreenTitleMenu","Description"..params.Choice)) end;
+			OnCommand=cmd(cropbottom,1;sleep,0.1;accelerate,0.3;cropbottom,0);
+			OffCommand=cmd(cropbottom,0;sleep,0.1;accelerate,0.3;cropbottom,1);
 		};
 	};
 };
