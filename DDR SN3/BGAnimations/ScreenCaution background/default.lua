28e37720b6f3d8d8bd554aa43bfe7bc76fed3ad3
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2015-11-01 09:20:04 -0600

    Flip texturerect dimensions

--- DDR SN3/BGAnimations/ScreenCaution background/default.lua
+++ DDR SN3/BGAnimations/ScreenCaution background/default.lua
@@ -6,9 +6,9 @@ return Def.ActorFrame{
 	LoadActor("fog_tiling")..{
 		InitCommand=cmd(Center;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT;diffusealpha,0.25;fadeleft,0.5;faderight,0.5;diffusecolor,color("#14FE00"));
 		OnCommand=function(self)
-			local w = self:GetWidth() / DISPLAY:GetDisplayWidth() ;
-			local h = self:GetHeight() / DISPLAY:GetDisplayHeight() ;
-			self:customtexturerect(0,0,w*1,h*1);
+			local w = DISPLAY:GetDisplayWidth() / self:GetWidth();
+			local h = DISPLAY:GetDisplayHeight() / self:GetHeight();
+			self:customtexturerect(0,0,w,h);
 			self:texcoordvelocity(0.2,0);
 			self:diffusealpha(1);
 			self:sleep(2);
