b6565329ff3c08799d03dee4c7fc92deccebbf97
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2015-11-01 01:10:57 -0500

    Tiling's a little better

--- DDR SN3/BGAnimations/ScreenCaution background/default.lua
+++ DDR SN3/BGAnimations/ScreenCaution background/default.lua
@@ -3,12 +3,12 @@ return Def.ActorFrame{
 		InitCommand=cmd(Center);
 		OnCommand=cmd(diffusealpha,1;sleep,2;linear,0.165;diffusealpha,0);
 	};
-	LoadActor(THEME:GetPathB("","ScreenLogo background/fog"))..{
+	LoadActor("fog_tiling")..{
 		InitCommand=cmd(Center;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT;diffusealpha,0.25;fadeleft,0.5;faderight,0.5;diffusecolor,color("#14FE00"));
 		OnCommand=function(self)
-			local w = DISPLAY:GetDisplayWidth() / self:GetWidth();
-			local h = DISPLAY:GetDisplayHeight() / self:GetHeight();
-			self:customtexturerect(0,0,w*0,h*1);
+			local w = self:GetWidth() / DISPLAY:GetDisplayWidth() ;
+			local h = self:GetHeight() / DISPLAY:GetDisplayHeight() ;
+			self:customtexturerect(0,0,w*1,h*1);
 			self:texcoordvelocity(0.2,0);
 			self:diffusealpha(1);
 			self:sleep(2);
