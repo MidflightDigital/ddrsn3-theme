2c2d57f8484455f64a1ae7305d09e922cbf42210
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2015-11-15 12:14:02 -0600

    Header text works and looks alright, man

--- DDR SN3/Graphics/ScreenWithMenuElements header/default.lua
+++ DDR SN3/Graphics/ScreenWithMenuElements header/default.lua
@@ -1,4 +1,19 @@
-return Def.ActorFrame{
+local screenName = Var "LoadingScreen"
+local headerTextImage
+
+--this should probably be a metric
+if screenName == "ScreenSelectMusic" then
+    headerTextImage = "Music.png"
+elseif string.find(screenName, "Options") then
+    headerTextImage = "Options.png"
+elseif screenName == "ScreenSelectStyle" then
+    headerTextImage = "Style.png"
+elseif string.find(screenName, "Evaluation") then
+    headerTextImage = "Results.png"
+end
+
+
+local out = Def.ActorFrame{
 	LoadActor("base")..{
 		InitCommand=cmd(x,SCREEN_LEFT;halign,0);
 	};
@@ -9,4 +24,14 @@ return Def.ActorFrame{
 		InitCommand=cmd(x,SCREEN_RIGHT-50;y,SCREEN_TOP+12;diffusealpha,0.5);
 		OnCommand=cmd(addy,-76;sleep,0.2;decelerate,0.2;addy,76;blend,Blend.Add;;spin;effectmagnitude,0,0,-200);
 	};
-};
+}
+
+if headerTextImage then
+	SCREENMAN:SystemMessage("Adding")
+	table.insert(out,LoadActor(headerTextImage)..{
+		InitCommand=function(self) self:x(SCREEN_LEFT+95):y(5):halign(0):valign(1) end,
+	})
+	SCREENMAN:SystemMessage("Alive")
+end
+
+return out
