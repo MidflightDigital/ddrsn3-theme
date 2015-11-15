412c8624728d498fe7a124fc6c68bfaaf8580c5b
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2015-11-15 15:59:53 -0600

    synced the animation of the menu timer to the fractional seconds available it also hides itself if the menu timer is disabled

--- DDR SN3/Graphics/ScreenWithMenuElements header/default.lua
+++ DDR SN3/Graphics/ScreenWithMenuElements header/default.lua
@@ -12,8 +12,19 @@ elseif string.find(screenName, "Evaluation") then
     headerTextImage = "Results.png"
 end
 
+local function UpdateAni(self)
+	local timer = SCREENMAN:GetTopScreen():GetChild "Timer"
+	local ani = self:GetChild "ani"
+	if timer and ani then
+		local time = timer:GetSeconds()
+		local ftime = time - math.floor(time)
+		ani:rotationz(360*ftime)
+	end
+end
+
 
 local out = Def.ActorFrame{
+	InitCommand=function(self) self:SetUpdateFunction(UpdateAni) end;
 	LoadActor("base")..{
 		InitCommand=cmd(x,SCREEN_LEFT;halign,0);
 	};
@@ -21,12 +32,17 @@ local out = Def.ActorFrame{
 		InitCommand=cmd(x,SCREEN_RIGHT-50;y,SCREEN_TOP+12);
 		OffCommand=cmd(bouncebegin,0.25;zoom,0);
 	};
-	LoadActor("ani")..{
-		InitCommand=cmd(x,SCREEN_RIGHT-50;y,SCREEN_TOP+12;diffusealpha,0.5);
-		OnCommand=cmd(addy,-76;sleep,0.2;decelerate,0.2;addy,76;blend,Blend.Add;;spin;effectmagnitude,0,0,-200);
-		OffCommand=cmd(stopeffect;bouncebegin,0.25;zoom,0);
-	};
 }
+
+if PREFSMAN:GetPreference("MenuTimer") == true then
+	table.insert(out,LoadActor("ani")..{
+		Name="ani";
+		InitCommand=cmd(x,SCREEN_RIGHT-50;y,SCREEN_TOP+12;diffusealpha,0.5);
+		OnCommand=cmd(addy,-76;sleep,0.2;decelerate,0.2;addy,76;blend,Blend.Add);
+		OffCommand=cmd(bouncebegin,0.25;zoom,0);
+	})
+
+end
 
 if headerTextImage then
 	table.insert(out,LoadActor(headerTextImage)..{
