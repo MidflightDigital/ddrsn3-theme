bde80450115ddf02dcb82a9f0fbbbf3f0fb286a8
Inorizushi <inorizushi@gmail.com>
2015-11-24 13:43:59 -0800

    nope
    yeah definitely not.

--- DDR SN3/BGAnimations/ScreenSelectMusic overlay/default.lua
+++ DDR SN3/BGAnimations/ScreenSelectMusic overlay/default.lua
@@ -1,7 +1,6 @@
 local t = Def.ActorFrame{
 	LoadActor("SNDifficultyList.lua");
 	LoadActor("bpmmeter.lua");
-	LoadActor(THEME:GetPathG("","MusicWheelItem grades/default.lua"));
 	LoadActor("labels")..{
 		InitCommand=cmd(x,SCREEN_LEFT+201;y,SCREEN_CENTER_Y+7);
 		OnCommand=cmd(draworder,2;diffusealpha,0;addx,-400;sleep,0.1;linear,0.52;diffusealpha,0;addx,400;sleep,0.3;diffusealpha,1);
