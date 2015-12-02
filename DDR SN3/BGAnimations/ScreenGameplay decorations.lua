3a33272411a1396897a4c7296bc1a28f344696ca
Inorizushi <inorizushi@gmail.com>
2015-12-02 14:47:09 -0800

    Fix scoring

--- DDR SN3/BGAnimations/ScreenGameplay decorations.lua
+++ DDR SN3/BGAnimations/ScreenGameplay decorations.lua
@@ -18,7 +18,7 @@ end
 t[#t+1] = Def.Actor{
     Name="ScoringController",
     JudgmentMessageCommand = function(_,params)
-        if not (( ScoringInfo[params.Player]) or 
+        if not (( ScoringInfo[params.Player]) and 
             (ScoringInfo.seed == GAMESTATE:GetStageSeed())) then 
             SN2Scoring.PrepareScoringInfo()
             ScoringInfo.seed = GAMESTATE:GetStageSeed() 
@@ -37,7 +37,7 @@ t[#t+1] = Def.Actor{
 }
 
 t[#t+1] = LoadActor( THEME:GetPathB("","optionicon_P1") ) .. {
-	InitCommand=cmd(player,PLAYER_1;zoom,1.4;x,WideScale(SCREEN_LEFT+140,SCREEN_LEFT+320);draworder,1);
+	InitCommand=cmd(player,PLAYER_1;zoom,1.4;x,WideScale(SCREEN_LEFT+140,SCREEN_LEFT+140);draworder,1);
 	OnCommand=function(self)
 		if GAMESTATE:PlayerIsUsingModifier(PLAYER_1,'reverse') then
 			self:y(SCREEN_TOP+70);
@@ -47,7 +47,7 @@ t[#t+1] = LoadActor( THEME:GetPathB("","optionicon_P1") ) .. {
 	end;
 };
 t[#t+1] = LoadActor( THEME:GetPathB("","optionicon_P2") ) .. {
-	InitCommand=cmd(player,PLAYER_2;zoom,1.4;x,WideScale(SCREEN_RIGHT-140,SCREEN_LEFT-320);draworder,1);
+	InitCommand=cmd(player,PLAYER_2;zoom,1.4;x,WideScale(SCREEN_RIGHT-140,SCREEN_RIGHT-140);draworder,1);
 	OnCommand=function(self)
 		if GAMESTATE:PlayerIsUsingModifier(PLAYER_2,'reverse') then
 			self:y(SCREEN_TOP+70);
