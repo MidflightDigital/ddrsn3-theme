dec49a9598a9932c98ac5a8b3a7261eff2e17ac0
Inorizushi <inorizushi@gmail.com>
2015-12-07 14:17:08 -0800

    Update default.lua

--- DDR SN3/BGAnimations/ScreenNameEntry background/default.lua
+++ DDR SN3/BGAnimations/ScreenNameEntry background/default.lua
@@ -1,4 +1,4 @@
 -- in 2004, Meriken DDR are invaded by ITG. no one is laughin.
 return Def.ActorFrame{
-	LoadActor("nameentry")..{ InitCommand=cmd(Center) };
+	LoadActor(THEME:GetPathB("","ScreenWithMenuElements background/default.lua"));
 };
