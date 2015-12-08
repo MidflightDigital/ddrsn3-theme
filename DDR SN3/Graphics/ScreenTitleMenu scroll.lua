81ea6db49572669420b90acaf9ae41bc28f4d452
Inorizushi <inorizushi@gmail.com>
2015-12-08 14:08:37 -0800

    Make the title cursor fancier (if it isn't broken that is.)

--- DDR SN3/Graphics/ScreenTitleMenu scroll.lua
+++ DDR SN3/Graphics/ScreenTitleMenu scroll.lua
@@ -4,8 +4,8 @@ return Def.ActorFrame {
 	Def.ActorFrame{
 		LoadActor("title cursor2")..{
 			OnCommand=cmd(halign,0;addx,-114;cropright,0.9;sleep,0.2;linear,0.3;cropright,0;faderight,0.2);
-			GainFocusCommand=cmd(stoptweening;linear,0.125;diffusealpha,0.5;);
-			LoseFocusCommand=cmd(stoptweening;linear,0.125;diffusealpha,0;);
+			GainFocusCommand=cmd(stoptweening;cropright,1;addx,10;linear,0.125;diffusealpha,0.5;cropright,0;addx,-10);
+			LoseFocusCommand=cmd(stoptweening;linear,0.125;addx,10;diffusealpha,0;);
 			OffCommand=cmd(linear,0.3;cropright,1);
 		};
 	};
@@ -20,7 +20,7 @@ return Def.ActorFrame {
 	Def.ActorFrame{
 		LoadActor("title cursor.png")..{
 			OnCommand=cmd(cropright,0.9;addx,114;sleep,0.2;linear,0.3;addx,-114;cropright,0;faderight,0.2);
-			GainFocusCommand=cmd(stoptweening;linear,0.125;diffusealpha,0.5;);
+			GainFocusCommand=cmd(stoptweening;cropright,1;addx,-10;linear,0.125;cropright,0;diffusealpha,0.5;addx,10);
 			LoseFocusCommand=cmd(stoptweening;linear,0.125;diffusealpha,0);
 			OffCommand=cmd(linear,0.3;cropright,1);
 		};
