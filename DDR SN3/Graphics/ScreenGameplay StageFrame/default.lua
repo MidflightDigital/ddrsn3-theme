cfdd8410fec9edb27205296e59facb5d4b0fb2ed
Inorizushi <inorizushi@gmail.com>
2015-12-08 13:45:37 -0800

    Fix Stage Frame
    I'm a smarticle

--- DDR SN3/Graphics/ScreenGameplay StageFrame/default.lua
+++ DDR SN3/Graphics/ScreenGameplay StageFrame/default.lua
@@ -3,10 +3,10 @@ local stageFrame = "normal"
 
 -- todo: show oni on life meter battery as well
 if GAMESTATE:GetPlayMode() == 'PlayMode_Oni' then
-	lifeFrame = "oni"
+	stageFrame = "oni"
 end
 
-if GAMESTATE:GetPlayMode() == 'PlayMode_Rave' then lifeFrame = "rave" end
-if GAMESTATE:IsExtraStage() or GAMESTATE:IsExtraStage2() then lifeFrame = "extra" end
+if GAMESTATE:GetPlayMode() == 'PlayMode_Rave' then stageFrame = "rave" end
+if GAMESTATE:IsExtraStage() or GAMESTATE:IsExtraStage2() then stageFrame = "extra" end
 
 return Def.ActorFrame{ LoadActor(stageFrame) };
