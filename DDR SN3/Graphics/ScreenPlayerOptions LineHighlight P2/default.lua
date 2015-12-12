5ed0353b9ef64da6ac347f086087f6bb55dc7c2e
Inorizushi <inorizushi@gmail.com>
2015-12-11 18:20:53 -0800

    doodledoo

---
+++ DDR SN3/Graphics/ScreenPlayerOptions LineHighlight P2/default.lua
@@ -0,0 +1,17 @@
+local t = ...;
+t = Def.ActorFrame{
+	LoadActor("Colour.png")..{
+	InitCommand=cmd(blend,Blend.Add;;diffusealpha,0.5);
+	};
+	Def.Sprite {
+	Texture="ScreenPlayerOptions LineHighlight P1 1x2";
+	
+	Frame0000=0;
+	Delay0000=0.5;
+	
+	Frame0001=1;
+	Delay0001=0.5;
+	InitCommand=cmd(addy,1;addx,200);
+	};	
+};	
+return t;
