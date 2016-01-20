12527686d84ec963cb2000311af6b5c2364830a4
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-01-20 12:08:16 -0600

    it fickerey

--- DDR SN3/Graphics/ScreenGameplay LifeFrame/default.lua
+++ DDR SN3/Graphics/ScreenGameplay LifeFrame/default.lua
@@ -3,10 +3,11 @@
 -- The math.floor(10000*aspect) trick is used to circumvent float precision problems.
 local aspectRatioSuffix = {
 	[math.floor(10000*4/3)] = " standard",
-	[math.floor(10000*16/9)] = " widescreen"
+	[math.floor(10000*16/9)] = " widescreen",
+	[math.floor(10000*16/10)] = " widescreen"
 }
 --fall back on the 4:3 frame if we don't know about this aspect ratio at all
-setmetatable(aspectRatioSuffix,{__index=function() return "" end})
+setmetatable(aspectRatioSuffix,{__index=function() return " standard" end})
 local suffix = aspectRatioSuffix[math.floor(10000*PREFSMAN:GetPreference("DisplayAspectRatio"))]
 
 local lifeFrame = "normal"
@@ -24,9 +25,12 @@ if ResolveRelativePath(lifeFrame..suffix,1,true) then
 	lifeFrame = lifeFrame .. suffix
 else
 	Warn("ScreenGameplay LifeFrame: missing frame \""..lifeFrame..suffix.."\". Using fallback assets.")
+	lifeFrame = lifeFrame.." standard"
 end
 
-return LoadActor(lifeFrame)..{
+local t = Def.ActorFrame{}
+t[#t+1] = LoadActor("flicker")
+t[#t+1] = LoadActor(lifeFrame)..{
 		BeginCommand=function(self,param)
 			if not GAMESTATE:IsPlayerEnabled('PlayerNumber_P1') then
 				self:cropleft(0.5)
@@ -35,3 +39,5 @@ return LoadActor(lifeFrame)..{
 			end
 		end;
 };
+
+return t
---
+++ DDR SN3/Graphics/ScreenGameplay LifeFrame/flicker.lua
@@ -0,0 +1,35 @@
+local function GetMetricNumber(name)
+    return tonumber(THEME:GetMetric("ScreenGameplay",name))
+end
+local flickerState = false
+local function HotLifeUpdate(self, _)
+    flickerState = not flickerState
+    for pn, item in pairs(self:GetChildren()) do
+        item:visible((GAMESTATE:GetPlayerState(pn):GetHealthState() == 'HealthState_Hot')
+            and flickerState)
+    end
+end
+local host = Def.ActorFrame{
+    Name = "HotLifeFlicker",
+    InitCommand = function(self) self:SetUpdateFunction(HotLifeUpdate) end;
+}
+
+local xPosPlayer = {
+    P1 = -157, 
+    P2 = 157
+}
+
+for _, pn in pairs(GAMESTATE:GetEnabledPlayers()) do
+    table.insert(host,Def.Quad{
+        Name = pn,
+        InitCommand=function(self)
+            local short = ToEnumShortString(pn)
+            self:visible(false):setsize((SCREEN_WIDTH/2.53),13)
+            :skewx(-0.9):diffuse(color "#FFFFFF"):x(xPosPlayer[short])
+            :halign(short=="P1" and 0.75 or -0.75)
+        end,
+        OnCommand=function(s) s:draworder(3):zoomx(pn=='PlayerNumber_P2' and -1 or 1) end,
+        OffCommand=function(s) s:sleep(0.792):addy(999) end
+    })
+end
+return host
