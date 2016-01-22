0dc69fd85946518430d31ddc3e1a7b88de2b465c
Inorizushi <inorizushi@gmail.com>
2016-01-22 08:00:35 -0800

    Toasty hookups
    Not even sure if this'll work.

--- DDR SN3/BGAnimations/ScreenGameplay toasty/default.lua
+++ DDR SN3/BGAnimations/ScreenGameplay toasty/default.lua
@@ -3,11 +3,11 @@ local charP2 = GAMESTATE:GetCharacter('PlayerNumber_P2'):GetDisplayName();
 
 local t = Def.ActorFrame {};
 
-if FILEMAN:DoesFileExist(THEME:GetCurrentThemeDirectory() .. "BGAnimations/ScreenGameplay toasty/" .. charP1 .. "/default.lua") and GAMESTATE:IsPlayerEnabled("PlayerNumber_P1") then
+if FILEMAN:DoesFileExist(THEME:GetCurrentThemeDirectory() .. "BGAnimations/ScreenGameplay toasty/char.lua") and GAMESTATE:IsPlayerEnabled("PlayerNumber_P1") then
 t[#t+1] = Def.ActorFrame {
 	LoadActor(charP1);
 };
-elseif FILEMAN:DoesFileExist(THEME:GetCurrentThemeDirectory() .. "BGAnimations/ScreenGameplay toasty/" .. charP2 .. "/default.lua") and GAMESTATE:IsPlayerEnabled("PlayerNumber_P2") then
+elseif FILEMAN:DoesFileExist(THEME:GetCurrentThemeDirectory() .. "BGAnimations/ScreenGameplay toasty/char.lua") and GAMESTATE:IsPlayerEnabled("PlayerNumber_P2") then
 t[#t+1] = Def.ActorFrame {
 	LoadActor(charP2);
 };
