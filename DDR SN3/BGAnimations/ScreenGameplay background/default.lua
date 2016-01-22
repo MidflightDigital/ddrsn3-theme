e0bd9f99f582d1b2b3fa920f488b23138e228cdd
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-01-22 14:36:37 -0600

    Restructure ScreenGameplay background

--- DDR SN3/BGAnimations/ScreenGameplay background/default.lua
+++ DDR SN3/BGAnimations/ScreenGameplay background/default.lua
@@ -1,32 +1,24 @@
 local t = Def.ActorFrame{};
 
+local function setVisibility(self)
+	local song = GAMESTATE:GetCurrentSong();
+	local shouldShowBGScripts = false
+	if song then
+		shouldShowBGScripts = not song:HasBGChanges()
+		if shouldShowBGScripts then
+			local opts = GAMESTATE:GetSongOptionsObject('ModsLevel_Current')
+			shouldShowBGScripts = not opts:StaticBackground()
+		end
+	end
+	SCREENMAN:GetTopScreen():GetChild("SongBackground"):visible(not shouldShowBGScripts);
+	self:visible(shouldShowBGScripts);
+end
+
 if CHARMAN:GetCharacterCount() >=1 then
 t[#t+1] = LoadActor("../BGScripts")..{
-	OnCommand=function(self)
-	local song = GAMESTATE:GetCurrentSong();
-	if song then
-		if GAMESTATE:PlayerIsUsingModifier(PLAYER_1,'StaticBG') or GAMESTATE:PlayerIsUsingModifier(PLAYER_2,'StaticBG') or song:HasBGChanges() then
-		SCREENMAN:GetTopScreen():GetChild("SongBackground"):visible(true);
-		self:visible(false);
-		else
-		SCREENMAN:GetTopScreen():GetChild("SongBackground"):visible(false);
-		self:visible(true);
-		end;
-		end;
-	end;
-	CurrentSongChangedMessageCommand=function(self)
-	local song = GAMESTATE:GetCurrentSong();
-	if song then
-		if GAMESTATE:PlayerIsUsingModifier(PLAYER_1,'StaticBG') or GAMESTATE:PlayerIsUsingModifier(PLAYER_2,'StaticBG') or song:HasBGChanges() then
-		SCREENMAN:GetTopScreen():GetChild("SongBackground"):visible(true);
-		self:visible(false);
-		else
-		SCREENMAN:GetTopScreen():GetChild("SongBackground"):visible(false);
-		self:visible(true);
-		end;
-		end;
-	end;
-	};
+	OnCommand=setVisibility,
+	CurrentSongChangedMessageCommand=setVisibility
+};
 end
 
 return t;
