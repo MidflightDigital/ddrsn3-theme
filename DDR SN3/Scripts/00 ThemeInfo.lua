23b04215b4ada654b0c42721b622ef98351882a2
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-01-30 01:00:18 -0600

    PUSH LEETLE CART!

--- DDR SN3/Scripts/00 ThemeInfo.lua
+++ DDR SN3/Scripts/00 ThemeInfo.lua
@@ -2,7 +2,7 @@
 themeInfo = {
 ProductCode = "DDR SuperNOVA 3",
 Name = "DDR SN3",
-Version = "alpha1.1",
-Date = "201601DD",
+Version = "alpha 2",
+Date = "2016MMDD",
 Author = "MidflightDigital"
 }
