13775ae69e891d479097c71bf87f8f9efbe32737
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-01-30 19:40:46 -0600

    Fixed select profile not appearing in non-Home modes and added that Blue Thing on sel music

--- DDR SN3/Graphics/MusicWheel highlight/default.lua
+++ DDR SN3/Graphics/MusicWheel highlight/default.lua
@@ -1,5 +1,22 @@
 local t = Def.ActorFrame {
 	LoadActor("Backing");
+    Def.Sprite{
+    Texture=THEME:GetPathG("MusicWheelItem Song","NormalPart/score");
+    InitCommand=function(s) s:x(-155) end;
+    CurrentSongChangedMessageCommand=function(self,params)
+    local song = GAMESTATE:GetCurrentSong();
+        if song then
+            if not PROFILEMAN:IsSongNew(song) then
+            self:visible(true);
+            self:draworder(1);
+            else
+            self:visible(false);
+            end;
+        else
+            self:visible(false);
+        end
+    end;
+};
 };
 
 return t;
