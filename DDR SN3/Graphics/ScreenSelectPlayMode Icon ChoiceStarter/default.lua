b9ced9a1bbbb681cf19a45b284a4457fa778c13a
Inorizushi <inorizushi@gmail.com>
2016-01-30 18:41:44 -0800

    god dammit.

--- DDR SN3/Graphics/ScreenSelectPlayMode Icon ChoiceStarter/default.lua
+++ DDR SN3/Graphics/ScreenSelectPlayMode Icon ChoiceStarter/default.lua
@@ -6,6 +6,12 @@ local t = Def.ActorFrame{
 		LoseFocusCommand=cmd(visible,false);
 		OffCommand=cmd(linear,0.133;addx,SCREEN_WIDTH);
 	};
+	LoadActor("badge")..{
+		InitCommand=cmd(x,SCREEN_CENTER_X+280;y,SCREEN_CENTER_Y+20);
+		GainFocusCommand=cmd(zoomx,0;zoomy,0;visible,true;linear,0.05;zoomx,1;sleep,0.2;linear,0.1;zoomy,1;glowshift;effectcolor1,color("1,1,1,0.2");effectcolor2,color("0,0,0,0");effectperiod,1);
+		LoseFocusCommand=cmd(visible,false);
+		OffCommand=cmd(linear,0.133;addx,SCREEN_WIDTH);
+	};
 };
 
 return t;
