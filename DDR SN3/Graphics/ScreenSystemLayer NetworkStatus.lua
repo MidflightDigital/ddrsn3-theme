a02fb98170c8960bb3272bd259e9d405168fa5fb
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-01-31 10:13:34 -0600

    i guess i just fucked with network status a little bit

--- DDR SN3/Graphics/ScreenSystemLayer NetworkStatus.lua
+++ DDR SN3/Graphics/ScreenSystemLayer NetworkStatus.lua
@@ -2,15 +2,14 @@ local netConnected = IsNetConnected();
 local loggedOnSMO = IsNetSMOnline();
 
 local t = Def.ActorFrame{
-	LoadFont("_network") .. {
-		InitCommand=cmd(uppercase,true;zoom,0.75;shadowlength,1);
+	LoadFont("ScreenSystemLayer Credits") .. {
+		InitCommand=cmd(uppercase,true;zoom,1;shadowlength,1);
 		BeginCommand=function(self)
 			-- check network status
 			if netConnected then
 				self:strokecolor(color("#000000"));
 				self:settext( Screen.String("Network OK") );
 			else
-				self:diffusecolor(color("#898989"))
 				self:strokecolor(color("#000000"));
 				self:settext( Screen.String("Offline") );
 			end;
