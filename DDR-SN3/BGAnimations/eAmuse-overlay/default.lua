76cb85feec8d6bce2a21832d00a3f17c21099562
Inorizushi <inorizushi@gmail.com>
2016-02-26 08:17:36 -0800

    fuck

--- DDR-SN3/BGAnimations/eAmuse-overlay/default.lua
+++
@@ -1,8 +0,0 @@
-local t = Def.ActorFrame{
-  LoadActor("box")..{
-    InitCommand=cmd(Center);
-    OnCommand=cmd(diffuseshift;effectcolor1,color("1,1,1,1");effectcolor2,color("0.5,0.5,0.5,1");
-  };
-};
-
-return t;
