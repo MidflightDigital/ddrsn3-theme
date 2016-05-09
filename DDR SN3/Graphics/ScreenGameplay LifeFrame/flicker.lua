9ac5a223d3b543864c91a6511dc819c31af08c21
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-05-08 20:15:12 -0500

    Change HotLifeFlicker to be time-based
    should provide a better effect with vsync off

--- DDR SN3/Graphics/ScreenGameplay LifeFrame/flicker.lua
+++ DDR SN3/Graphics/ScreenGameplay LifeFrame/flicker.lua
@@ -1,14 +1,15 @@
 local flickerState = false
-local function HotLifeUpdate(self, _)
-    flickerState = not flickerState
-    for pn, item in pairs(self:GetChildren()) do
-        item:visible((GAMESTATE:GetPlayerState(pn):GetHealthState() == 'HealthState_Hot')
-            and flickerState)
-    end
-end
 local host = Def.ActorFrame{
     Name = "HotLifeFlicker",
-    InitCommand = function(self) self:SetUpdateFunction(HotLifeUpdate) end;
+    InitCommand = function(self) self:queuecommand("HotLifeUpdate") end;
+    HotLifeUpdateCommand = function(self)
+        flickerState = not flickerState
+        for pn, item in pairs(self:GetChildren()) do
+            item:visible((GAMESTATE:GetPlayerState(pn):GetHealthState() == 'HealthState_Hot')
+                and flickerState)
+        end
+        self:sleep(1/60):queuecommand("HotLifeUpdate")
+    end,
 }
 
 local xPosPlayer = {
@@ -22,7 +23,7 @@ for _, pn in pairs(GAMESTATE:GetEnabledPlayers()) do
         InitCommand=function(self)
             local short = ToEnumShortString(pn)
             self:visible(false):setsize((SCREEN_WIDTH/2.53),13)
-            :skewx(-0.9):diffuse(color "0.5,0.5,0.5,0.8"):x(xPosPlayer[short])
+            :skewx(-0.9):diffuse(color "0.75,0.75,0.75,0.8"):x(xPosPlayer[short])
             :halign(0.75)
         end,
         OnCommand=function(s) s:draworder(3):zoomx(pn=='PlayerNumber_P2' and -1 or 1) end,
