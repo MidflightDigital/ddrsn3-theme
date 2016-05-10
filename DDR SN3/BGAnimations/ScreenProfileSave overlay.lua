341610006fcaf49669fbbef7300ab51cd0057209
Flameshadowxeroshin <Flameshadowxeroshin@gmail.com>
2016-05-09 19:58:57 -0500

    faster profile save?

--- DDR SN3/BGAnimations/ScreenProfileSave overlay.lua
+++ DDR SN3/BGAnimations/ScreenProfileSave overlay.lua
@@ -12,7 +12,6 @@ MyGrooveRadar.SaveAllRadarData()
 
 x[#x+1] = Def.Actor {
 	BeginCommand=function(self)
-		if SCREENMAN:GetTopScreen():HaveProfileToSave() then self:sleep(0.5); end;
 		self:queuecommand("Load");
 	end;
 	LoadCommand=function() SCREENMAN:GetTopScreen():Continue(); end;
