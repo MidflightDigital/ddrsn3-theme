0c9c630633b3b6c64f5a41d5b8318c7b35f325d9
Inorizushi <inorizushi@gmail.com>
2016-06-13 21:18:28 -0700

    Update shit with new logos and shit.

--- DDR SN3/BGAnimations/ScreenCredits overlay/default.lua
+++ DDR SN3/BGAnimations/ScreenCredits overlay/default.lua
@@ -10,10 +10,20 @@ t[#t+1] = Def.ActorFrame{
 	};
 };
 t[#t+1] = Def.ActorFrame{
-	OnCommand=cmd(diffusealpha,0;sleep,3;addx,300;accelerate,1;diffusealpha,0.8;addx,-300;sleep,2;diffusealpha,0);
+	OnCommand=cmd(diffusealpha,0;sleep,3;addx,300;accelerate,1;diffusealpha,0.8;addx,-300;sleep,2;linear,0.5;diffusealpha,0);
 	LoadFont("_handelgothic bt 20px")..{
 		InitCommand=function(self)
 			self:settext("Project Managers\n     HypnoticMarten77\n     Dreamland200\n\nGraphics Team\n     Inorizushi\n     HypnoticMarten77")
+			:x(SCREEN_RIGHT-280):halign(0)
+			:y(SCREEN_CENTER_Y-160):valign(0)
+		end;
+	};
+};
+t[#t+1] = Def.ActorFrame{
+	OnCommand=cmd(diffusealpha,0;sleep,6;addx,300;accelerate,1;diffusealpha,0.8;addx,-300;sleep,2;linear,0.5;diffusealpha,0);
+	LoadFont("_handelgothic bt 20px")..{
+		InitCommand=function(self)
+			self:settext("Stepping Team\n     Dreamland200\n     hypnoticmarten77\n     CuzcoBlocko\n     darkanine\n     Dancefreak\n     JunkoXXX\n     Nezemarth\n     Silverhawke\n     Tartatuchus\n     VocaloidFandom\n     Kowalski")
 			:x(SCREEN_RIGHT-280):halign(0)
 			:y(SCREEN_CENTER_Y-160):valign(0)
 		end;
