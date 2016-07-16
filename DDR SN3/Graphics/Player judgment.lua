2b356b45790ee81efb8ff75ffed05dad72bae02d
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2016-07-15 19:58:33 -0500

    Just display in Starter mode

--- DDR SN3/Graphics/Player judgment.lua
+++ DDR SN3/Graphics/Player judgment.lua
@@ -13,6 +13,15 @@ local JudgeCmds = {
 	TapNoteScore_Miss = THEME:GetMetric( "Judgment", "JudgmentMissCommand" );
 };
 
+local StJudgeCmds = {
+	TapNoteScore_W1 = THEME:GetMetric( "Judgment", "JudgmentW1Command" );
+	TapNoteScore_W2 = THEME:GetMetric( "Judgment", "JudgmentW1Command" );
+	TapNoteScore_W3 = THEME:GetMetric( "Judgment", "JudgmentW3Command" );
+	TapNoteScore_W4 = THEME:GetMetric( "Judgment", "JudgmentW3Command" );
+	TapNoteScore_W5 = THEME:GetMetric( "Judgment", "JudgmentMissCommand" );
+	TapNoteScore_Miss = THEME:GetMetric( "Judgment", "JudgmentMissCommand" );
+};
+
 local TNSFrames = {
 	TapNoteScore_W1 = 0;
 	TapNoteScore_W2 = 1;
@@ -21,9 +30,26 @@ local TNSFrames = {
 	TapNoteScore_W5 = 4;
 	TapNoteScore_Miss = 5;
 };
+
+--frame 1 is the Early frame, 2 is the Late frame and doesn't appear in the table
+--it is added by code downstream
+local StTNSFrames = {
+	TapNoteScore_W1 = 0;
+	TapNoteScore_W2 = 0;
+	TapNoteScore_W3 = 1;
+	TapNoteScore_W4 = 1;
+	TapNoteScore_W5 = 3;
+	TapNoteScore_Miss = 3;	
+};
+
+local env = GAMESTATE:Env();
+local starterMode = env.StarterMode == true;
+local activeFrames = starterMode and StTNSFrames or TNSFrames;
+local activeCmds = starterMode and StJudgeCmds or JudgeCmds;
+
 local t = Def.ActorFrame {};
 t[#t+1] = Def.ActorFrame {
-	LoadActor(THEME:GetPathG("Judgment","Normal")) .. {
+	LoadActor(THEME:GetPathG("Judgment",starterMode and "Starter" or "Normal")) .. {
 		Name="Judgment";
 		InitCommand=cmd(pause;visible,false);
 		OnCommand=THEME:GetMetric("Judgment","JudgmentOnCommand");
@@ -39,18 +65,20 @@ t[#t+1] = Def.ActorFrame {
 		if param.HoldNoteScore then return end;
 
 		local iNumStates = c.Judgment:GetNumStates();
-		local iFrame = TNSFrames[param.TapNoteScore];
+		local iFrame = activeFrames[param.TapNoteScore];
 		if not iFrame then return end
 		
 		local iTapNoteOffset = param.TapNoteOffset;
 		
-		--early/late code not yet here
+		if iFrame == 1 and iTapNoteOffset > 0 then
+			iFrame = 2;
+		end;
 		
 		self:playcommand("Reset");
 
 		c.Judgment:setstate( iFrame );
 		c.Judgment:visible( true );
-		JudgeCmds[param.TapNoteScore](c.Judgment);
+		activeCmds[param.TapNoteScore](c.Judgment);
 	end;
 };
 
