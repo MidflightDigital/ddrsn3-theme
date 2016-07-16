df5eadf459c41a356d1bbd2b97824b3a0b0b642d
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2016-07-15 19:03:20 -0500

    copy Player judgment.lua ITG style early/late crap removed because we won't use that. Protiming stuff also removed, because if we do add protiming what is there is not enough and we should probably do it all over.

---
+++ DDR SN3/Graphics/Player judgment.lua
@@ -0,0 +1,58 @@
+--Player judgment.lua
+--From _fallback, author unclear
+--Stripped and remodeled for DDR SN3
+local c;
+local player = Var "Player";
+
+local JudgeCmds = {
+	TapNoteScore_W1 = THEME:GetMetric( "Judgment", "JudgmentW1Command" );
+	TapNoteScore_W2 = THEME:GetMetric( "Judgment", "JudgmentW2Command" );
+	TapNoteScore_W3 = THEME:GetMetric( "Judgment", "JudgmentW3Command" );
+	TapNoteScore_W4 = THEME:GetMetric( "Judgment", "JudgmentW4Command" );
+	TapNoteScore_W5 = THEME:GetMetric( "Judgment", "JudgmentW5Command" );
+	TapNoteScore_Miss = THEME:GetMetric( "Judgment", "JudgmentMissCommand" );
+};
+
+local TNSFrames = {
+	TapNoteScore_W1 = 0;
+	TapNoteScore_W2 = 1;
+	TapNoteScore_W3 = 2;
+	TapNoteScore_W4 = 3;
+	TapNoteScore_W5 = 4;
+	TapNoteScore_Miss = 5;
+};
+local t = Def.ActorFrame {};
+t[#t+1] = Def.ActorFrame {
+	LoadActor(THEME:GetPathG("Judgment","Normal")) .. {
+		Name="Judgment";
+		InitCommand=cmd(pause;visible,false);
+		OnCommand=THEME:GetMetric("Judgment","JudgmentOnCommand");
+		ResetCommand=cmd(finishtweening;stopeffect;visible,false);
+	};
+	
+	InitCommand = function(self)
+		c = self:GetChildren();
+	end;
+
+	JudgmentMessageCommand=function(self, param)
+		if param.Player ~= player then return end;
+		if param.HoldNoteScore then return end;
+
+		local iNumStates = c.Judgment:GetNumStates();
+		local iFrame = TNSFrames[param.TapNoteScore];
+		if not iFrame then return end
+		
+		local iTapNoteOffset = param.TapNoteOffset;
+		
+		--early/late code not yet here
+		
+		self:playcommand("Reset");
+
+		c.Judgment:setstate( iFrame );
+		c.Judgment:visible( true );
+		JudgeCmds[param.TapNoteScore](c.Judgment);
+	end;
+};
+
+
+return t;
