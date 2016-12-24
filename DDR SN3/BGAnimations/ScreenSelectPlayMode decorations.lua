4d4a718a2cdc89d5442c7bd9a11e90b472f973fc
Inorizushi <inorizushi@gmail.com>
2016-12-24 04:05:07 -0800

    A couple graphical fixes

--- DDR SN3/BGAnimations/ScreenSelectPlayMode decorations.lua
+++ DDR SN3/BGAnimations/ScreenSelectPlayMode decorations.lua
@@ -6,6 +6,7 @@ t[#t+1] = LoadActor(THEME:GetPathS("","_swoosh"))..{
 
 t[#t+1] = Def.Sprite{
 	InitCommand=cmd(scaletoclipped,427,240;xy,SCREEN_LEFT+240,SCREEN_CENTER_Y-40);
+	OnCommand=cmd(addx,-640;sleep,0.116;accelerate,0.25;addx,640);
 	Anim1Command=cmd(linear,0.2;diffuse,color("0,0,0,1"));
 	Anim2Command=cmd(linear,0.2;diffuse,color("1,1,1,1"));
 	PlayStarterMessageCommand=function(s) s:queuecommand("Anim1"):Load(THEME:GetPathG("","_PlayMode/Standard")):queuecommand("Anim2") end,
@@ -13,6 +14,7 @@ t[#t+1] = Def.Sprite{
 	PlayBattleMessageCommand=function(s) s:queuecommand("Anim1"):Load(THEME:GetPathG("","_PlayMode/Battle")):queuecommand("Anim2") end,
 	PlayCourseMessageCommand=function(s) s:queuecommand("Anim1"):Load(THEME:GetPathG("","_PlayMode/Course")):queuecommand("Anim2") end,
 	PlayEndlessMessageCommand=function(s) s:queuecommand("Anim1"):Load(THEME:GetPathG("","_PlayMode/Endless")):queuecommand("Anim2") end,
+	OffCommand=cmd(sleep,0.116;accelerate,0.25;addx,-640);
 };
 t[#t+1] = LoadActor(THEME:GetPathG("","_footer/confirm"))..{
 	InitCommand=cmd(draworder,199;x,SCREEN_RIGHT-71;y,SCREEN_BOTTOM-35;);
