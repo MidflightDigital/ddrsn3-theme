ba569f4e093c3747aaa4d2316f7adeca5fcdfd98
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2017-01-01 21:07:58 -0600

    okay I think I actually got it this time also the ThemeInfo.ini name for the theme matches the window title now

--- DDR SN3/BGAnimations/ScreenDemonstration background.lua
+++ DDR SN3/BGAnimations/ScreenDemonstration background.lua
@@ -2,14 +2,18 @@
 
 local t = Def.ActorFrame{};
 
+local visibilityOverride = nil
+
 local function setVisibility(self)
     local song = GAMESTATE:GetCurrentSong();
-    local shouldShowBGScripts = false
-    if song then
-        shouldShowBGScripts = not song:HasBGChanges()
-        if shouldShowBGScripts then
-            local opts = GAMESTATE:GetSongOptionsObject('ModsLevel_Current')
-            shouldShowBGScripts = not opts:StaticBackground()
+    local shouldShowBGScripts = visibilityOverride or false
+    if visibilityOverride == nil then
+        if song then
+            shouldShowBGScripts = not song:HasBGChanges()
+            if shouldShowBGScripts then
+                local opts = GAMESTATE:GetSongOptionsObject('ModsLevel_Current')
+                shouldShowBGScripts = not opts:StaticBackground()
+            end
         end
     end
     local bg = SCREENMAN:GetTopScreen():GetChild("SongBackground")
@@ -30,12 +34,12 @@ if #vids > 0 then
     local danceVid = nil
     repeat
         --Chooses one of the dancers
-        local choose = table.remove(vids,math.random(1,#vids))
+        local choose = table.remove(vids,(#vids == 1) and 1 or math.random(1,#vids))
 
         --Sets the Dancer to be loaded
         local charName = vids[choose]
 
-        local danceVid = Characters.GetDancerVideo(charName)
+        danceVid = Characters.GetDancerVideo(charName)
     until (danceVid ~= nil) or (#vids == 0)
 
     if danceVid then
@@ -44,7 +48,7 @@ if #vids > 0 then
 	   InitCommand=cmd(draworder,1;Center;zoomto,SCREEN_WIDTH+38,SCREEN_HEIGHT+38;);	
         };
     else
-        self:visible(false);
+        visibilityOverride = false
     end
 end
 
