c0e57fffcea3cac1c176872c790885b0c1b21d5b
Inorizushi <inorizushi@gmail.com>
2017-01-03 19:13:01 -0800

    Add proper shop and mission mode music

--- DDR SN3/BGAnimations/ScreenShop decorations.lua
+++ DDR SN3/BGAnimations/ScreenShop decorations.lua
@@ -2,6 +2,9 @@ local t = Def.ActorFrame{
   LoadActor("closed")..{
     InitCommand=cmd(Center);
   };
+  LoadFont("_handelgothic bt 20px")..{
+    InitCommand=cmd(zoom,1.2;settext,"I'll Buy It At a High Price!\n- DJ Vortivask";halign,1;x,SCREEN_RIGHT-40;y,SCREEN_TOP+40);
+  };
 };
 
 return t;
