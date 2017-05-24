48d5c86a02f81df1f1ba1a5bbed2ff6ce0e79007
Inorizushi <inorizushi@gmail.com>
2017-05-24 12:56:31 -0700

    Fix the menu music

--- DDR SN3/Sounds/ScreenSelectPlayMode music.lua
+++ DDR SN3/Sounds/ScreenSelectPlayMode music.lua
@@ -2,8 +2,14 @@ local Music = ""
 
 if ThemePrefs.Get("MenuMusic") == "IIDX SIRIUS" then
   Music = "_SIRIUS MODE (loop).ogg"
-else
-  Music = "_common menu music.lua"
+elseif ThemePrefs.Get("MenuMusic") == "SuperNOVA" then
+  Music = "_sn1 (loop).ogg"
+elseif ThemePrefs.Get("MenuMusic") == "SuperNOVA 2" then
+  Music = "_sn2 (loop).ogg"
+elseif ThemePrefs.Get("MenuMusic") == "SuperNOVA 3" then
+  Music = "_sn3 (loop).ogg"
+elseif ThemePrefs.Get("MenuMusic") == "EXTREME" then
+  Music = "_extreme (loop).ogg"
 end;
 
 return THEME:GetPathS("",Music);
