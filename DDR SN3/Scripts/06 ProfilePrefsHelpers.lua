1417c8c91e6219a1f113dc5fc30804dc66e2d953
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2017-06-04 10:59:01 -0500

    fix filter (@here please test w/o profiles)

--- DDR SN3/Scripts/06 ProfilePrefsHelpers.lua
+++ DDR SN3/Scripts/06 ProfilePrefsHelpers.lua
@@ -3,18 +3,31 @@ local entryToPrefixMap = {
 	['character'] = "SNCharacter"
 }
 
+local function dbglog(msg)
+	if SN3Debug then
+		return print("LoadFromProfilePrefs: "..msg)
+	end
+end
+
 function LoadFromProfilePrefs()
+	dbglog("start")
 	--note: unless you don't use the _fallback version of getenv/setenv this
 	--code does not work
 	local env = GAMESTATE:Env()
+	if env.setupPerformed then
+		dbglog("already ran this game")
+		return
+	end
+	dbglog "first time through"
+	env.setupPerformed = true
 	for _, pn in pairs(GAMESTATE:GetEnabledPlayers()) do
 		local prefs = ProfilePrefs.Read(GetProfileIDForPlayer(pn))
 		local shortPn = ToEnumShortString(pn)
 		for sourceName, destName in pairs(entryToPrefixMap) do
 			destName = destName..shortPn
-			if not env[destName] then
-				env[destName] = prefs[sourceName]
-			end
+			dbglog("setting "..destName.." to "..tostring(prefs[sourceName]))
+			env[destName] = prefs[sourceName]
+			dbglog("getenv gives "..tostring(getenv(destName)))
 		end
 	end
 end 
