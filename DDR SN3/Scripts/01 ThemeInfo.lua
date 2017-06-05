36f980d06b2f780a004c8c2dbdd90f449fb1cded
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2017-06-05 18:49:58 -0500

    bump version number for r6 to r6.2 note: this is not any indication that i want to do an r6.2 release soon or ever, but if we do another r6 release that is what it will be

--- DDR SN3/Scripts/01 ThemeInfo.lua
+++ DDR SN3/Scripts/01 ThemeInfo.lua
@@ -2,7 +2,7 @@
 themeInfo = {
 ProductCode = "DDR SuperNOVA 3",
 Name = "DDR SuperNOVA 3",
-Version = "r6.1",
-Date = "2017060x",
+Version = "r6.2",
+Date = "2017xxxx",
 Author = "MidflightDigital"
 }
