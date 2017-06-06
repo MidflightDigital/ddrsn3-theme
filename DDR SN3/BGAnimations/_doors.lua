b50a7094952d4f2b7d51a81a0dac336ed03672bc
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2017-06-05 22:39:26 -0500

    it's visible now but not fixed

--- DDR SN3/BGAnimations/_doors.lua
+++ DDR SN3/BGAnimations/_doors.lua
@@ -1,4 +1,4 @@
-local imagePrefix, speed, isClose = ...
+local imagePrefix, speed, isClose, twoPart = ...
 if not (imagePrefix	and speed and (isClose~=nil)) then
 	Warn("_doors malfunction")
 	Warn(debug.traceback())
@@ -14,12 +14,26 @@ local t = Def.ActorFrame {
 		LoadActor(THEME:GetPathB("",imagePrefix..'2'))..{
 		InitCommand=cmd(y,coordinates.bottom.starting;x,SCREEN_CENTER_X;valign,1);
 			OnCommand=cmd(accelerate,speed;y,coordinates.bottom.ending);
+			OffCommand=function(s)
+				if twoPart then
+					s:accelerate(speed):y(coordinates.bottom.starting)
+				end
+			end;
+			StartCommand=function(s) s:queuecommand("On") end;
+			FinishCommand=function(s) s:queuecommand("Off") end;
 		};
 	};
 	Def.ActorFrame{
 		LoadActor(THEME:GetPathB("",imagePrefix..'1'))..{
 		InitCommand=cmd(y,coordinates.top.starting;x,SCREEN_CENTER_X;valign,0);
 			OnCommand=cmd(accelerate,speed;y,coordinates.top.ending);
+			OffCommand=function(s)
+				if twoPart then
+					s:accelerate(speed):y(coordinates.top.starting)
+				end
+			end;
+			StartCommand=function(s) s:queuecommand("On") end;
+			FinishCommand=function(s) s:queuecommand("Off") end;
 		};
 	};
 };
