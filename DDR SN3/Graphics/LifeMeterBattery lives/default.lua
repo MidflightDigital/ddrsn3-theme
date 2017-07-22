ee85ae35daa855fc61862bc3897765a569929371
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2017-07-21 23:53:46 -0500

    godf bless

--- DDR SN3/Graphics/LifeMeterBattery lives/default.lua
+++ DDR SN3/Graphics/LifeMeterBattery lives/default.lua
@@ -4,9 +4,12 @@ local curLives = nil
 local lastLives = nil
 
 local stream = "normal"
+local lives = GAMESTATE:GetPlayerState(player):GetPlayerOptions('ModsLevel_Current'):BatteryLives()
 
-if GAMESTATE:GetPlayerState(player):GetPlayerOptions('ModsLevel_Current'):BatteryLives() >= 4 then
+if lives >= 4 then
 	stream = "hot"
+elseif lives == 1 then
+	stream = "danger"
 end
 
 local t = Def.ActorFrame {};
@@ -21,7 +24,11 @@ t[#t+1] = Def.ActorFrame{
 		end;
 		LifeChangedMessageCommand=function(self,params)
 			if params.LostLife and params.Player == player then
-				self:Load(THEME:GetPathG("StreamDisplay","normal"))
+				if params.LivesLeft == 1 then
+					self:Load(THEME:GetPathG("StreamDisplay","danger"))
+				elseif params.LivesLeft < 4 then
+					self:Load(THEME:GetPathG("StreamDisplay","normal"))
+				end
 			end
 		end;
 	};
@@ -35,9 +42,7 @@ t[#t+1] = Def.ActorFrame{
 			self:x(SCREEN_WIDTH/5);
 		end;
 		BeginCommand=function(self,params)
-			local screen = SCREENMAN:GetTopScreen();
-			local glifemeter = screen:GetLifeMeter(player);
-			self:setsize((SCREEN_WIDTH/10.12)*(4-math.min(4,glifemeter:GetTotalLives())), 13);
+			self:setsize((SCREEN_WIDTH/10.12)*(4-math.min(4,lives)), 13);
 		end;
 		LifeChangedMessageCommand=function(self,params)
 			if params.Player ~= player then return end;
