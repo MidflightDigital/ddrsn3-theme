8fc8dcbe0ff35c219eab457e5e2dd3f32a5d32f9
Inorizushi <inorizushi@gmail.com>
2017-10-01 00:40:15 -0700

    Fix Alice X3 configs, re-add eval music

--- SNCharacters/aliceX3/config.lua
+++ SNCharacters/aliceX3/config.lua
@@ -1,6 +1,6 @@
 return {
     --version must be 1.
-    version = 2,
+    version =3,
     --color must be a table or string.
     --You can't use the color() function here because this file runs in an
     --empty environment, but you don't need to since the config loader does
