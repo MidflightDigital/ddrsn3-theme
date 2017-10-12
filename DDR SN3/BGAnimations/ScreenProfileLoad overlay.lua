84ab221ebe13de64eae282256a7e7bc91f0a29e0
Inorizushi <inorizushi@gmail.com>
2017-10-12 13:50:51 -0700

    rollback combo fonts, add new badges

--- DDR SN3/BGAnimations/ScreenProfileLoad overlay.lua
+++ DDR SN3/BGAnimations/ScreenProfileLoad overlay.lua
@@ -3,7 +3,7 @@ local x = Def.ActorFrame{};
 x[#x+1] = Def.Actor {
 	BeginCommand=function(self)
 		if SCREENMAN:GetTopScreen():HaveProfileToLoad() then self:sleep(1);  end;
-		self:sleep(2):queuecommand("Load");
+		self:sleep(1):queuecommand("Load");
 	end;
 	LoadCommand=function() SCREENMAN:GetTopScreen():Continue(); end;
 };
