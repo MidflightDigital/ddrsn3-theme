f7617fea9522c1437a3143e8861aad41d8846fcc
Inorizushi <inorizushi@gmail.com>
2018-03-20 01:58:40 -0700

    Add some fonts

--- DDR SN3/BGAnimations/ScreenCredits overlay/default.lua
+++ DDR SN3/BGAnimations/ScreenCredits overlay/default.lua
@@ -6,7 +6,7 @@ local t = Def.ActorFrame{
 };
 
 t[#t+1] = Def.ActorFrame{
-	LoadFont("_handelgothic bt 20px")..{
+	LoadFont("_futura std medium 20px")..{
 		InitCommand=function(self)
 			self:settext("DanceDanceRevolution SuperNOVA 3")
 			:zoom(1.5)
@@ -17,7 +17,7 @@ t[#t+1] = Def.ActorFrame{
 };
 t[#t+1] = Def.ActorFrame{
 	OnCommand=cmd(diffusealpha,0;sleep,3;addx,300;accelerate,1;diffusealpha,0.8;addx,-300;sleep,2;linear,0.5;diffusealpha,0);
-	LoadFont("_handelgothic bt 20px")..{
+	LoadFont("_futura std medium 20px")..{
 		InitCommand=function(self)
 			self:settext("Project Managers\n     HypnoticMarten77\n     Dreamland200\n\nGraphics Team\n     Inorizushi\n     HypnoticMarten77")
 			:x(SCREEN_RIGHT-280):halign(0)
@@ -27,7 +27,7 @@ t[#t+1] = Def.ActorFrame{
 };
 t[#t+1] = Def.ActorFrame{
 	OnCommand=cmd(diffusealpha,0;sleep,6;addx,300;accelerate,1;diffusealpha,0.8;addx,-300;sleep,2;linear,0.5;diffusealpha,0);
-	LoadFont("_handelgothic bt 20px")..{
+	LoadFont("_futura std medium 20px")..{
 		InitCommand=function(self)
 			self:settext("Stepping Team\n     Dreamland200\n     hypnoticmarten77\n     CuzcoBlocko\n     darkanine\n     Dancefreak\n     JunkoXXX\n     Nezemarth\n     Silverhawke\n     Tartatuchus\n     VocaloidFandom\n     Kowalski")
 			:x(SCREEN_RIGHT-280):halign(0)
